ed9c77d49aba3112f76205a360e84fed4a928af6
LiuXY1998 <1409824833@qq.com>
2019-10-10 20:21:30 +0800

    12

--- js/test.js
+++ js/test.js
@@ -107,3 +107,47 @@ window.addEventListener('load',function () {
     two.setOption(twoOption);
 },false);
 
+window.addEventListener('load',function () {
+    let main = document.querySelectorAll('.echarts3');
+    let three = echarts.init(main[0]);
+    let threeOption = {
+        color: ['#3398DB'],
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis : [
+            {
+                type : 'category',
+                data : ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                name:'',
+                type:'bar',
+                barWidth: '60%',
+                data:[7.2, 5.6, 12.3, 8.4, 6.5, 2.4, 5.7]
+            }
+        ]
+    };
+
+    three.setOption(threeOption);
+},false);
+
