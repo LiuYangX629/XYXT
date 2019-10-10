8c5b41c0394bc0866f18122a7a5345e43e49fd88
LiuXY1998 <1409824833@qq.com>
2019-10-10 16:10:46 +0800

    12

--- js/test.js
+++ js/test.js
@@ -1,7 +1,5 @@
 window.addEventListener('load',function () {
     let main = document.querySelectorAll('.echarts1');
-
-
     let one = echarts.init(main[0]);
     let oneOption = {
         xAxis: {
@@ -13,7 +11,7 @@ window.addEventListener('load',function () {
             type: 'value'
         },
         series: [{
-            data: [820, 932, 901, 934, 1290, 1330, 1320,820, 932, 901, 934, 1290, 1330,820, 932, 901, 934, 1290, 1330,820, 932, 901, 934, 1290],
+            data: [4.5, 4.5, 4.3,4.3, 4.2, 4.2, 4.3,4.2, 6.1, 6.2, 6.2, 6.0,7.1 ,7.2, 7.5, 7.2, 7.1, 7.0, 6.5,6.2, 5.6, 5.2, 5.0, 5.0],
             type: 'line',
             areaStyle: {}
         }]
@@ -23,23 +21,89 @@ window.addEventListener('load',function () {
 
 window.addEventListener('load',function () {
     let main = document.querySelectorAll('.echarts2');
-
     let two = echarts.init(main[0]);
+    var colors = ['#5793f3', '#d14a61', '#675bba'];
     let twoOption = {
-        xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: ['00:00', '01.00', '02.00', '03.00', '04.00', '05.00', '06.00','07.00','08.00','09.00','10.00','11.00','12.00','13.00','14.00','15.00','16.00','17.00','18.00','19.00','20.00','21.00','22.00','23.00']
-        },
-        yAxis: {
-            type: 'value'
-        },
-        series: [{
-            data: [420, 432, 401, 434, 490, 330, 420,420, 432, 401, 434, 490, 420, 432, 401, 434, 490, 330, 420,420, 432, 401, 434, 490],
-            type: 'line',
-            areaStyle: {}
-        }]
-    };
+        color: colors,
 
+        tooltip: {
+            trigger: 'none',
+            axisPointer: {
+                type: 'cross'
+            }
+        },
+        legend: {
+            data:['舒张压', '收缩压']
+        },
+        grid: {
+            top: 70,
+            bottom: 50
+        },
+        xAxis: [
+            {
+                type: 'category',
+                axisTick: {
+                    alignWithLabel: true
+                },
+                axisLine: {
+                    onZero: false,
+                    lineStyle: {
+                        color: colors[1]
+                    }
+                },
+                axisPointer: {
+                    label: {
+                        formatter: function (params) {
+                            return '收缩压  ' + params.value
+                                + (params.seriesData.length ? '：' + params.seriesData[0].data : '');
+                        }
+                    }
+                },
+                data: ['00:00', '01.00', '02.00', '03.00', '04.00', '05.00', '06.00','07.00','08.00','09.00','10.00','11.00','12.00','13.00','14.00','15.00','16.00','17.00','18.00','19.00','20.00','21.00','22.00','23.00']
+            },
+            {
+                type: 'category',
+                axisTick: {
+                    alignWithLabel: true
+                },
+                axisLine: {
+                    onZero: false,
+                    lineStyle: {
+                        color: colors[0]
+                    }
+                },
+                axisPointer: {
+                    label: {
+                        formatter: function (params) {
+                            return '舒张压  ' + params.value
+                                + (params.seriesData.length ? '：' + params.seriesData[0].data : '');
+                        }
+                    }
+                },
+                data: ['00:00', '01.00', '02.00', '03.00', '04.00', '05.00', '06.00','07.00','08.00','09.00','10.00','11.00','12.00','13.00','14.00','15.00','16.00','17.00','18.00','19.00','20.00','21.00','22.00','23.00']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name:'舒张压',
+                type:'line',
+                xAxisIndex: 1,
+                smooth: true,
+                data: [78,79,75,76,74,78,71,80,81,84,82,80,79,80,82,84,85,82,80,81,78,76,76,75]
+            },
+            {
+                name:'收缩压',
+                type:'line',
+                smooth: true,
+                data: [115,113,114,119,115,114,115,119,122,123,125,121,122,128,126,126,124,122,123,121,120,118,120,118]
+            }
+        ]
+    };
     two.setOption(twoOption);
 },false);
+
